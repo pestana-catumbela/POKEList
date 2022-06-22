5b09ca679cb8374c9125e70a173dbe829bdb3393
safare.ao <pestana-catumbela>
2022-06-22 17:07:23 -0300

    POKEList

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -4,23 +4,56 @@ import './body.css';
 
 export default class Body extends Component{
     state = {
-        pokeNames: [ ]
+        pokeNames: [ ],
+        // anterior: { },
+        seguinte: { }
     }
 
     componentDidMount(){
         this.buscPoke()
+        this.pageNext()
     }
 
     async buscPoke(){
         try{
             const res = await Api.get()
             const pokeNames = res.data.results
-            this.setState({pokeNames})
+            // const anterior = {
+            //     previous: res.data.previous
+            // }
+            const seguinte = {
+                next: res.data.next
+            }
+            this.setState({pokeNames, seguinte})
             console.log(this.state.pokeNames)
         }
         catch{
             console.log("Erro!...")
         }
+    }
+
+    // pagePrevious = () =>{
+    //     const {anterior} = this.state;
+    //     if(anterior.previous === null) return
+    //     const pagePrevious = anterior.previous
+    //     this.buscPoke(pagePrevious);
+    // }
+
+    pageNext = () => {
+        try{
+            const {seguinte} = this.state;
+            if(seguinte.next !== null){
+                const pagePrevious = seguinte.next
+                this.buscPoke(pagePrevious);
+            }
+        }
+        catch{
+            console.log('Erro...')
+        }
+        // if(seguinte !== null){
+        //     const pageNext = seguinte
+        //     console.log(pageNext)
+        // }
     }
 
     render(){
@@ -33,9 +66,9 @@ export default class Body extends Component{
                         </strong>
 
                         <div className="btns">
-                            <button type="submit" className="btn1">Anterior</button>
+                            <button className="btn1" onClick={this.pagePrevious}>Anterior</button>
                             <span>Página 1 de 100</span>
-                            <button type="submit" className="btn2">Prosterior</button>
+                            <button className="btn2" onClick={this.pageNext}>Seguinte</button>
                         </div>
                     </div>
                 </section>
